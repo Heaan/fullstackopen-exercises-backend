0758d370a5abebd469499eaa9daed3a953cb9cb7
Heaan <cxy.eml@live.cn>
2020-05-15 19:52:57 +0800

    my phonebook heroku app add the statuc file

---
+++ build/precache-manifest.33098530876403471d3504835d39ff32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15978e18a5ff4740a9389e5f7f568afe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03f7959f1fa4e23ba5ac",
+    "url": "/static/css/main.bddd1c33.chunk.css"
+  },
+  {
+    "revision": "b14df7992e95a1a6bafb",
+    "url": "/static/js/2.d8d29dd6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8d29dd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03f7959f1fa4e23ba5ac",
+    "url": "/static/js/main.d76b6802.chunk.js"
+  },
+  {
+    "revision": "6be06fbc24171e2fd061",
+    "url": "/static/js/runtime-main.110b1f99.js"
+  }
+]);
