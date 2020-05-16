7bba82dd100443a9f5162056d7d0728ccf429a29
Heaan <cxy.eml@live.cn>
2020-05-16 23:12:42 +0800

    add part3 exercise 3.15

--- index.js
+++ index.js
@@ -83,9 +83,14 @@ app.get("/api/persons/:id", (req, res) => {
 
 app.delete("/api/persons/:id", (req, res) => {
   const id = req.params.id;
-  Person.findByIdAndRemove(id).then((result) => {
-    res.status(204).end();
-  });
+  Person.findByIdAndRemove(id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send({ error: "malformatted id" });
+    });
 });
 
 app.post("/api/persons", (req, res) => {
